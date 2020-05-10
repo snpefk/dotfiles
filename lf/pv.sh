0b94150122dc381127bca485cf1f9b19b24c1078
snpefk <snpefk@gmail.com>
2020-05-10 12:34:10 +0300

    lf: update pv.sh

--- lf/pv.sh
+++ lf/pv.sh
@@ -5,10 +5,10 @@ case "$1" in
 	*.tar.bz2|*.tbz2) tar tjf "$1";;
 	*.tar.txz|*.txz) xz --list "$1";;
     *.tar) tar tf "$1";;
-	*.zip|*.jar|*.war|*.ear|*.oxt) unzip -l "$1";;
+	*.zip|*.jar|*.war|*.ear|*.oxt|*.siq) unzip -l "$1";;
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
-	*.md) glow "$1";;
+	*.md) glow -s dark "$1";;
     *.pdf) pdftotext "$1" -;;
     *.jpg) w3m "$1";;
 	*.o) nm "$1" | less ;;
@@ -18,5 +18,5 @@ case "$1" in
 	*.doc) catdoc "$1" ;;
 	*.docx) docx2txt "$1" - ;;
 	*.csv) cat "$1" | sed s/,/\\n/g ;;
-    *) highlight -O ansi "$1" || bat "$1";;
+    *) bat --color=always --theme=base16 --style=changes "$1";;
 esac
